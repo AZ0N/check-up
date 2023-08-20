c1f266a61172bd2157ff2bf57371a57663eb860a
Mads Nielsen <madscwn@gmail.com>
2023-08-20 15:04:08 +0200

    update duration formatting

--- lib/util.dart
+++ lib/util.dart
@@ -1,25 +1,40 @@
 class Util {
-  // Format duration using "X year, Y weeks, Z days, W hours",
-  // only displaying number X,Y,Z if they're different from 0
   static String formatDuration(Duration duration) {
     int years = duration.inDays ~/ 365;
     int weeks = (duration.inDays - years * 365) ~/ 7;
     int days = duration.inDays - (years * 365 + weeks * 7);
-    int hours = duration.inHours - (years * 365 + weeks * 7 + days) * 24;
+    int hours = duration.inHours - duration.inDays * 24;
+    int minutes = duration.inMinutes - duration.inHours * 60;
+    int seconds = duration.inSeconds - duration.inMinutes * 60;
 
+    // The number of time measures to use
+    // (years, weeks, days) or (weeks, days, hours) etc.
+    int count = 3;
     String result = "";
 
-    if (years > 0) {
+    if (count > 0 && years > 0) {
       result += "$years ${years == 1 ? "year" : "years"}, ";
+      count--;
     }
-    if (weeks > 0) {
+    if (count > 0 && weeks > 0) {
       result += "$weeks ${weeks == 1 ? "week" : "weeks"}, ";
+      count--;
     }
-    if (days > 0) {
+    if (count > 0 && days > 0) {
       result += "$days ${days == 1 ? "day" : "days"}, ";
+      count--;
     }
-    result += "$hours ${hours == 1 ? "hour" : "hours"} ";
-
+    if (count > 0 && hours > 0) {
+      result += "$hours ${hours == 1 ? "hour" : "hours"}, ";
+      count--;
+    }
+    if (count > 0 && minutes > 0) {
+      result += "$minutes ${minutes == 1 ? "minute" : "minutes"}, ";
+      count--;
+    }
+    if (count > 0) {
+      result += "$seconds ${seconds == 1 ? "second" : "seconds"}, ";
+    }
     return result;
   }
 }
